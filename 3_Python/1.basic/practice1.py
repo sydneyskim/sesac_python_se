cb99329bc9940ae8a8b0eae2aaa00754fdbbfaad
sydneyskim <seung2332@gmail.com>
2024-05-21 22:44:58 +0900

    basic 폴더 변경

--- 3_Python/1.basic/practice1.py
+++ 3_Python/1.basic/practice1.py
@@ -1,41 +1,65 @@
 # 001 print 기초
 # 화면에 Hello World 문자열을 출력하세요.
 
+# print('Hello World')
+
 # 002 print 기초
 # 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
+
+# print("Mary's cosmetics")
 
 # 003 print 기초
 # 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)
 # 신씨가 소리질렀다. "도둑이야".
 
+# print('신씨가 소리질렀다. "도둑이야".')
+
 # 004 print 기초
 # 화면에 C:\Windows를 출력하세요.
+
+# print('C:\Windows')
 
 # 005 print 탭과 줄바꿈
 # 다음 코드를 실행해보고 \t와 \n의 역할을 설명해보세요.
 # print("안녕하세요.\n만나서\t\t반갑습니다.")
 
+# \t->탭 \n 줄바꿈
+
 # 006 print 여러 데이터 출력
 # print 함수에 두 개의 단어를 입력한 예제입니다. 아래 코드의 출력 결과를 예상해봅시다.
 # print ("오늘은", "일요일")
+
+#오늘은 일요일
 
 # 007 print 기초
 # print() 함수를 사용하여 다음과 같이 출력하세요.
 # naver;kakao;sk;samsung
 
+# print('naver','kakao','sk','samsung', sep=";")
+
 # 008 print 기초
 # print() 함수를 사용하여 다음과 같이 출력하세요.
 # naver/kakao/sk/samsung
+
+# print('naver','kakao','sk','samsung', sep="/")
 
 # 009 print 줄바꿈
 # 다음 코드를 수정하여 줄바꿈이 없이 출력하세요. (힌트: end='') print 함수는 두 번 사용합니다. 세미콜론 (;)은 한줄에 여러 개의 명령을 작성하기 위해 사용합니다.
 # print("first");print("second")
 
+# print("first", end="");print("second")
+
 # 010 연산 결과 출력
 # 5/3의 결과를 화면에 출력하세요.
 
+# print(5/3)
+
 # 011 변수 사용하기
 # 삼성전자라는 변수로 50,000원을 바인딩해보세요. 삼성전자 주식 10주를 보유하고 있을 때 총 평가금액을 출력하세요.
+
+# 삼성전자 = 50000
+# 총평가금액 = 삼성전자 * 10
+# print(총평가금액)
 
 # 012 변수 사용하기
 # 다음 표는 삼성전자의 일부 투자정보입니다. 변수를 사용해서 시가총액, 현재가, PER 등을 바인딩해보세요.
@@ -43,6 +67,13 @@
 # 시가총액	298조
 # 현재가	50,000원
 # PER	15.79
+
+# 시가총액 = 298000000000000
+# 현재가 = 50000
+# PER = 15.79
+# print(시가총액, type(시가총액))
+# print(현재가, type(현재가))
+# print(PER, type(PER))
 
 # 013 문자열 출력
 # 변수 s와 t에는 각각 문자열이 바인딩 되어있습니다.
@@ -52,9 +83,16 @@
 # 실행 예:
 # hello! python
 
+# s = "hello"
+# t = "python"
+# print(s + "! " + t)
+
 # 014 파이썬을 이용한 값 계산
 # 아래 코드의 실행 결과를 예상해보세요.
 # >> 2 + 2 * 3 
+
+# print(2 + 2 * 3)
+# 8
 
 # 015 type 함수
 # type() 함수는 데이터 타입을 판별합니다. 변수 a에는 128 숫자가 바인딩돼 있어 type 함수가 int (정수)형임을 알려줍니다.
@@ -64,23 +102,47 @@
 # 아래 변수에 바인딩된 값의 타입을 판별해보세요.
 # >> a = "132"
 
+# a = "132"
+# print(type(a))
+# <class 'str'>
+
 # 016 문자열을 정수로 변환
 # 문자열 '720'를 정수형으로 변환해보세요.
 # >> num_str = "720"
+
+# num_str = "720"
+# num_int = int(num_str)
+# print(num_int)
 
 # 017 정수를 문자열 100으로 변환
 # 정수 100을 문자열 '100'으로 변환해보세요.
 # num = 100
 
+# num = 100
+# str = str(num)
+# print(str)
+
 # 018 문자열을 실수로 변환
 # 문자열 "15.79"를 실수(float) 타입으로 변환해보세요.
+
+# str = "15.79"
+# flt = float(str)
+# print(flt)
 
 # 019 문자열을 정수로 변환
 # year라는 변수가 문자열 타입의 연도를 바인딩하고 있습니다. 이를 정수로 변환한 후 최근 3년의 연도를 화면에 출력해보세요.
 # year = "2020"
 
+# year = "2020"
+# print(int(year)-2)
+# print(int(year)-1)
+# print(int(year))
+
 # 020 파이썬 계산
 # 에이컨이 월 48,584원에 무이자 36개월의 조건으로 홈쇼핑에서 판매되고 있습니다. 총 금액은 계산한 후 이를 화면에 출력해보세요. (변수사용하기)
+
+# ac_monthly = 48584
+# print(ac_monthly*36)
 
 # 021 문자열 인덱싱
 # letters가 바인딩하는 문자열에서 첫번째와 세번째 문자를 출력하세요.
@@ -88,10 +150,16 @@
 # Copy실행 예
 # p t
 
+# lang = 'python'
+# print(lang[0], lang[2])
+
 # 022 문자열 슬라이싱
 # 자동차 번호가 다음과 같을 때 뒤에 4자리만 출력하세요.
 # >> license_plate = "24가 2210"
 # 실행 예: 2210
+
+# license_plate = "24가 2210"
+# print(license_plate[-4:])
 
 # 023 문자열 인덱싱
 # 아래의 문자열에서 '홀' 만 출력하세요.
@@ -99,11 +167,17 @@
 # 실행 예:
 # 홀홀홀
 
+# string = "홀짝홀짝홀짝"
+# print(string[::2])
+
 # 024 문자열 슬라이싱
 # 문자열을 거꾸로 뒤집어 출력하세요.
 # >> string = "PYTHON"
 # 실행 예:
 # NOHTYP
+
+# string = "PYTHON"
+# print(string[::-1])
 
 # 025 문자열 치환
 # 아래의 전화번호에서 하이푼 ('-')을 제거하고 출력하세요.
@@ -111,16 +185,28 @@
 # 실행 예
 # 010 1111 2222
 
+# phone_number = "010-1111-2222"
+# phone_number2 = phone_number.replace("-"," ")
+# print(phone_number2)
+
 # 026 문자열 다루기
 # 25번 문제의 전화번호를 아래와 같이 모두 붙여 출력하세요.
 # 실행 예
 # 01011112222
+
+# phone_number = "010-1111-2222"
+# phone_number2 = phone_number.replace("-","")
+# print(phone_number2)
 
 # 027 문자열 다루기
 # url 에 저장된 웹 페이지 주소에서 도메인을 출력하세요.
 # >> url = "http://sharebook.kr"
 # 실행 예:
 # kr
+
+# url = "http://sharebook.kr"
+# url_split = url.split('.')
+# print(url_split[-1])
 
 # 028 문자열은 immutable
 # 아래 코드의 실행 결과를 예상해보세요.
