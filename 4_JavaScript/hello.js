cd4cebd11881b8deb56ee4a6c12247cbb7f34800
sydneyskim <seung2332@gmail.com>
2024-06-03 16:44:46 +0900

    230603 수업내용

---
+++ 4_JavaScript/hello.js
@@ -0,0 +1,17 @@
+
+function sayhello() {
+    console.log('Hello')
+}
+    function sayprint() {
+        print()
+}
+
+console.log('콘솔로그') // 콘솔창에 로그를 출력한다. 
+document.write('Hello, World') // 문서에 글자를 쓴다.
+document.write('<P>')
+document.write('Hello, Again')
+document.write('<h2>헤딩추가하기<h2>')
+
+for (var i = 1; i <= 5; i++) {
+    document.write("<h3>숫자: " + i + "</h3>");
+}
