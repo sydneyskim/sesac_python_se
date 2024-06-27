820eb0956dd06d5c5c535aa377c33b15fc9ba21e
sydneyskim <seung2332@gmail.com>
2024-06-27 21:29:57 +0900

    페이징 1차수정완

--- Project/app.py
+++ Project/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
-from database import get_orders, get_user_by_name, get_users, get_user_by_gender, get_user_by_name_and_gender, get_orderItems, get_items, get_stores
-
+from database import get_users, get_total_users, get_user_by_name, get_user_by_gender, get_user_by_name_and_gender, get_total_user_by_name, get_total_user_by_gender, get_total_user_by_name_and_gender, get_orders, get_total_orders, get_orderItems, get_total_orderItems, get_items, get_total_items, get_stores, get_total_stores
+import math
 app = Flask(__name__)
+
 app.secret_key = "thisissydneysproject"
 # app.permanent_session_lifetime = timedelta(minutes=10)
 
@@ -18,42 +19,67 @@ def crm_users():
     
     if name and gender:
         users = get_user_by_name_and_gender(name, gender, page, per_page)
+        total_users = get_total_user_by_name_and_gender(name, gender, page, per_page)
+        total_pages = math.ceil(total_users / per_page)
     elif name:
         users = get_user_by_name(name, page, per_page)
+        total_users = get_total_user_by_name(name, page, per_page)
+        total_pages = math.ceil(total_users / per_page)
     elif gender:
         users = get_user_by_gender(gender, page, per_page)
+        total_users = get_total_user_by_gender(gender, page, per_page)
+        total_pages = math.ceil(total_users / per_page)
     else:
         users = get_users(page, per_page)
+        total_users = get_total_users()
+        total_pages = math.ceil(total_users / per_page)
     
-    return render_template('users.html', users=users, page=page, per_page=per_page)
+    return render_template('users.html', users=users, page=page, per_page=per_page, total_pages=total_pages)
 
 @app.route('/crm/orders/')
 def crm_orders():
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=10, type=int)
+    
+    total_orders = get_total_orders()
+    total_pages = math.ceil(total_orders / per_page)
+    
     orders = get_orders(page, per_page)
-    return render_template('orders.html', orders=orders, page=page, per_page=per_page)
+    
+    return render_template('orders.html', orders=orders, page=page, per_page=per_page, total_pages=total_pages)
 
 @app.route('/crm/orderItems/')
 def crm_orderItems():
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=10, type=int)
+    
+    total_orderItems = get_total_orderItems()
+    total_pages = math.ceil(total_orderItems / per_page)
+
     orderItems = get_orderItems(page, per_page)
-    return render_template('orderItems.html', orderItems=orderItems, page=page, per_page=per_page)
+    return render_template('orderItems.html', orderItems=orderItems, page=page, per_page=per_page, total_pages=total_pages)
 
 @app.route('/crm/items/')
 def crm_items():
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=10, type=int)
+    
+    total_items = get_total_items()
+    total_pages = math.ceil(total_items / per_page)
+    
     items = get_items(page, per_page)
-    return render_template('items.html', items=items, page=page, per_page=per_page)
+    return render_template('items.html', items=items, page=page, per_page=per_page, total_pages=total_pages)
 
 @app.route('/crm/stores/')
 def crm_stores():
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=10, type=int)
+    
+    total_stores = get_total_stores()
+    total_pages = math.ceil(total_stores / per_page)
+    
     stores = get_stores(page, per_page)
-    return render_template('stores.html', stores=stores, page=page, per_page=per_page)
+    return render_template('stores.html', stores=stores, page=page, per_page=per_page, total_pages=total_pages)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- Project/database.py
+++ Project/database.py
@@ -15,68 +15,104 @@ def get_query(query, params=()):
     result = [dict(row) for row in result]
     conn.close() # 연결 닫기
     
-    # 첫 번째 행이 헤더라면 제외
-    if result and result[0].get('Id') == 'Id':
-        result = result[1:]
-    
     return result # 결과 반환
 
+### Users ###
+# user 전체 가져오기
 def get_users(page, per_page):
     offset = (page - 1) * per_page
-    query = """
-    SELECT * FROM (
-        SELECT *, ROW_NUMBER() OVER (ORDER BY ROWID) as row_num FROM users
-    ) WHERE row_num > 1 LIMIT ? OFFSET ?
-    """
+    query = "SELECT * FROM users LIMIT ? OFFSET ?"
     return get_query(query, (per_page, offset))
 
+# user 전체 count
+def get_total_users():
+    query = "SELECT COUNT(*) as count FROM users"
+    result = get_query(query)
+    return result[0]['count'] if result else 0
+
+# user 이름 검색결과
 def get_user_by_name(name, page, per_page):
     offset = (page - 1) * per_page
     query = "SELECT * FROM users WHERE Name LIKE ? LIMIT ? OFFSET ?"
     return get_query(query, ('%' + name + '%', per_page, offset))
 
+# user 이름 검색결과 count
+def get_total_user_by_name(name, page, per_page):
+    query = "SELECT COUNT(*) as count FROM users WHERE Name LIKE ?"
+    result = get_query(query, ('%' + name + '%',))
+    return result[0]['count'] if result else 0
+
+# user 성별 검색결과
 def get_user_by_gender(gender, page, per_page):
     offset = (page - 1) * per_page
     query = "SELECT * FROM users WHERE Gender = ? LIMIT ? OFFSET ?"
     return get_query(query, (gender, per_page, offset))
 
+# user 성별 검색결과 count
+def get_total_user_by_gender(gender):
+    query = "SELECT COUNT(*) as count FROM users WHERE Gender = ?"
+    result = get_query(query, (gender,))
+    return result[0]['count'] if result else 0
+
+# user 이름&성별 검색결과
 def get_user_by_name_and_gender(name, gender, page, per_page):
     offset = (page - 1) * per_page
     query = "SELECT * FROM users WHERE Name LIKE ? AND Gender = ? LIMIT ? OFFSET ?"
     return get_query(query, ('%' + name + '%', gender, per_page, offset))
 
+# user 이름&성별 검색결과 count
+def get_total_user_by_name_and_gender(name, gender):
+    query = "SELECT COUNT(*) as count FROM users WHERE Name LIKE ? AND Gender = ?"
+    result = get_query(query, ('%' + name + '%', gender))
+    return result[0]['count'] if result else 0
+
+### Orders ###
+# order 전체 가져오기
 def get_orders(page, per_page):
     offset = (page - 1) * per_page
-    query = """
-    SELECT * FROM (
-        SELECT *, ROW_NUMBER() OVER (ORDER BY ROWID) as row_num FROM orders
-    ) WHERE row_num > 1 LIMIT ? OFFSET ?
-    """
+    query = "SELECT * FROM orders LIMIT ? OFFSET ?"
     return get_query(query, (per_page, offset))
+# order 전체 count
+def get_total_orders():
+    query = "SELECT COUNT(*) as count FROM orders"
+    result = get_query(query)
+    return result[0]['count'] if result else 0
 
+### OrderItems ###
+# orderItem 전체 가져오기
 def get_orderItems(page, per_page):
     offset = (page - 1) * per_page
-    query = """
-    SELECT * FROM (
-        SELECT *, ROW_NUMBER() OVER (ORDER BY ROWID) as row_num FROM orderitems
-    ) WHERE row_num > 1 LIMIT ? OFFSET ?
-    """
+    query = "SELECT * FROM orderItems LIMIT ? OFFSET ?"
     return get_query(query, (per_page, offset))
 
+# orderItem 전체 count
+def get_total_orderItems():
+    query = "SELECT COUNT(*) as count FROM orderItems"
+    result = get_query(query)
+    return result[0]['count'] if result else 0
+
+### Items ###
+# item 전체 가져오기
 def get_items(page, per_page):
     offset = (page - 1) * per_page
-    query = """
-    SELECT * FROM (
-        SELECT *, ROW_NUMBER() OVER (ORDER BY ROWID) as row_num FROM items
-    ) WHERE row_num > 1 LIMIT ? OFFSET ?
-    """
+    query = "SELECT * FROM items LIMIT ? OFFSET ?"
     return get_query(query, (per_page, offset))
 
+# item 전체 count
+def get_total_items():
+    query = "SELECT COUNT(*) as count FROM items"
+    result = get_query(query)
+    return result[0]['count'] if result else 0
+
+### Stores ###
+# store 전체 가져오기
 def get_stores(page, per_page):
     offset = (page - 1) * per_page
-    query = """
-    SELECT * FROM (
-        SELECT *, ROW_NUMBER() OVER (ORDER BY ROWID) as row_num FROM stores
-    ) WHERE row_num > 1 LIMIT ? OFFSET ?
-    """
+    query = "SELECT * FROM stores LIMIT ? OFFSET ?"
     return get_query(query, (per_page, offset))
+
+# store 전체 count
+def get_total_stores():
+    query = "SELECT COUNT(*) as count FROM stores"
+    result = get_query(query)
+    return result[0]['count'] if result else 0
